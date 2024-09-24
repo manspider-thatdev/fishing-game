8d2c5147ab1ac11625b74a08fd924389e3746ae4
Watson <34805464+CasualWatson@users.noreply.github.com>
2024-09-24 17:52:11 -0500

    Add Baby Fish

---
+++ scripts/fish.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@onready var life_timer: Timer = $Sprite2D/LifespanT
+@export var lifespan: float = 10.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	life_timer.start(lifespan)
+	print(lifespan)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_lifespan_t_timeout() -> void:
+	queue_free()
+	print("Fish go bye-bye!")
