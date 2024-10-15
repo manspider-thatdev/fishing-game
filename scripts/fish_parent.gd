bb2acfc4cbd2ae8e9220a5b02be53e9ecbf5a14f
LoganLHarris <46304597+LoganLHarris@users.noreply.github.com>
2024-10-15 17:25:44 -0500

    Remove NextCheck timer
    Godot docs said its best to program your own timer instead of using a Timer node for very short times, so I'm doing that.

--- scripts/fish_parent.gd
+++ scripts/fish_parent.gd
@@ -1,19 +1,24 @@
 extends Node2D
 
-@onready var time_until_next_check = $NextCheck
+@onready var time_since_last_check = 0;
 @onready var fish = preload("res://scenes/fish.tscn")
+@onready var fish_count = 0
 
 const FISH_CAP = 5
 const TIME_BETWEEN_CHECKS = 0.05
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	time_until_next_check.wait_time = TIME_BETWEEN_CHECKS
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	time_since_last_check += delta
+	# Check if fish cap has been met each interval.
+	if(time_since_last_check >= TIME_BETWEEN_CHECKS):
+		#spawn_fish()
+		time_since_last_check = 0
 
 #TODO: Create a function that spawns fish.
 func spawn_fish():
