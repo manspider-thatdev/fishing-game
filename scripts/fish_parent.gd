15c38eb2cddff097cf84ca2835f93427fd767d7e
LoganLHarris <46304597+LoganLHarris@users.noreply.github.com>
2024-10-15 17:03:41 -0500

    added wip fish spawning

--- scripts/fish_parent.gd
+++ scripts/fish_parent.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+@onready var time_until_next_check = $NextCheck
+@onready var fish = preload("res://scenes/fish.tscn")
+
+const FISH_CAP = 5
+const TIME_BETWEEN_CHECKS = 0.05
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	time_until_next_check.wait_time = TIME_BETWEEN_CHECKS
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+#TODO: Create a function that spawns fish.
+func spawn_fish():
 	pass
