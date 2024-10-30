83e268f49fe8663faabb1c6a168a40768eb90986
CasualWatson <34805464+CasualWatson@users.noreply.github.com>
2024-10-29 22:53:32 -0500

    Fish Spawn Gaming!!!

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -3,101 +3,97 @@ class_name Fish
 
 enum FishStates {ROAM, SEEK, HOOK, FLEE}
 var fish_state: FishStates = FishStates.ROAM
-
-class FishData: # for instantiation
-	var is_immortal: bool = false
-	var lifespan: float = 60.0
-	var time_until_move: float = 3.0
-	var move_speed: float = 40.0
-	var flee_wait: float = time_until_move
-	var texture2D: Texture2D = preload("res://assets/2d/placeholder fish/FishRed.png")
-
-var fish_data: FishData
+var fish_data: FishData = FishData.new() # see: fish_data.gd, usually should change w/ set_values() below
 var rng := RandomNumberGenerator.new()
 
-@onready var screensize: Vector2 = get_viewport().get_visible_rect().size
+@onready var screensize: Vector2 = get_viewport().size
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var life_timer: Timer = $Timers/Lifespan
 @onready var move_timer: Timer = $Timers/Movement
 @onready var flee_timer: Timer = $Timers/Fleeing
-@onready var idle_target: Node2D = $TargetPos # typed as Node2D to allow swapping for bobber later
-@onready var sprite: Sprite2D = $Sprite2D
-@onready var current_target: Node2D = idle_target
+@onready var idle_target: Node2D = $TargetPos
+@onready var current_target: Node2D = idle_target # typed as Node2D to allow swapping for bobber
 var target_position:
 	get():
+		sprite.flip_h = true if (current_target.position - position).x < 0 else false
 		return current_target.position
+
+# Update the FishData class in fish_data.gd to add more parameters
+func set_values(start_position: Vector2, data: FishData):
+	fish_data = data
+	position = start_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	assert(fish_data, "[fish_data] is null.")
 	sprite.texture = fish_data.texture2D
-	new_target()
+	new_idle_target()
 	move_timer.start(fish_data.time_until_move)
 	if fish_data.is_immortal == false:
 		life_timer.start(fish_data.lifespan)
 
-# Sets the values of the fish instance. Update the FishData class above for more needed parameters
-func set_values(new_position: Vector2, new_data: FishData = FishData.new()):
-	fish_data.is_immortal = new_data.is_immortal
-	fish_data.lifespan = new_data.lifespan
-	fish_data.time_until_move = new_data.time_until_move
-	fish_data.move_speed = new_data.move_speed
-	position = new_position
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if life_timer.time_left == 0 and !is_immortal:
-		if fish_state != FishStates.SEEK && fish_state != FishStates.HOOK:
-			queue_free()
+	match fish_state:
+		FishStates.HOOK:
+			anim_player.play("ROAM", -1, 4)
+		FishStates.ROAM:
+			if life_timer.time_left == 0 and !fish_data.is_immortal:
+				queue_free()
+			anim_player.play("ROAM")
+		FishStates.SEEK:
+			anim_player.play("ROAM", -1, 1.25)
+		FishStates.FLEE:
+			if life_timer.time_left == 0 and !fish_data.is_immortal:
+				queue_free()
+			anim_player.play("ROAM", -1, 2)
 
-func set_target(pos: Vector2) -> void:
-	idle_target.position = pos
-
-func new_target() -> void:
+func new_idle_target() -> Node2D:
 	var direction := Vector2(rng.randf_range(-1, 1), rng.randf_range(-1, 1))
-	var potential := time_until_move * move_speed
+	var potential := fish_data.time_until_move * fish_data.move_speed
 	var magnitude := rng.randf_range(0, potential) # adds variety by not always going max-distance
 	var t_pos := direction * magnitude
 	# Out-of-bounds Checking/Handling
 	t_pos = t_pos.clamp(Vector2.ZERO, screensize)
 	idle_target.position = t_pos
-
+	print("Fish wants to move to: %s" % target_position)
+	return idle_target
 
 func _physics_process(delta: float) -> void:
 	match fish_state:
 		FishStates.HOOK: # Presumably do nothing while QTEs go off
 			pass
 		FishStates.ROAM:
-			position = position.move_toward(target_position, move_speed * delta)
-		FishStates.SEEK: # Pursuing the bobber actively
-			position = position.move_toward(target_position, move_speed * delta)
-		FishStates.FLEE: # When startled, possibly tied to a signal from the bobber?
-			position = position.move_toward(target_position, -move_speed * delta)
-
+			position = position.move_toward(target_position, fish_data.move_speed * delta)
+		FishStates.SEEK: # Pursuing the bobber actively, see _on_bobber_move() below
+			position = position.move_toward(target_position, fish_data.move_speed * delta)
+		FishStates.FLEE: # When startled, see _on_bobber_move() below
+			position = position.move_toward(target_position, -fish_data.move_speed * delta)
 
 func _on_lifespan_timeout() -> void:
 	if fish_data.is_immortal:
 		life_timer.start(fish_data.lifespan)
-	elif fish_state != FishStates.SEEK && fish_state != FishStates.HOOK:
+	elif fish_state != FishStates.SEEK and fish_state != FishStates.HOOK:
 		queue_free()
 
-
 func _on_move_timeout() -> void:
-	new_target()
-	print("Fish wants to move to:")
-	print(current_target.position)
+	new_idle_target()
 
 func _on_fleeing_timeout() -> void:
-	new_target()
+	current_target = new_idle_target()
 	fish_state = FishStates.ROAM
-	current_target = idle_target
+	move_timer.paused = false
 
-# Future Signals Probably
+# Bobber Interactions
 func _on_catch() -> void: # When entering nearest bobber range and biting
 	fish_state = FishStates.HOOK
 
-func _on_bobber_move(bobber: Node2D, isScared: bool) -> void: # Check for Interest or Startle
+func _on_bobber_move(bobber: Node2D, isScared: bool) -> void: # Called by bobber.gd
 	current_target = bobber
 	if !isScared:
 		fish_state = FishStates.SEEK
 	elif isScared:
 		fish_state = FishStates.FLEE
+		move_timer.paused = true
 		flee_timer.start(fish_data.flee_wait)
--- scripts/fish_parent.gd
+++ scripts/fish_parent.gd
@@ -2,15 +2,17 @@ extends Node
 
 var rng := RandomNumberGenerator.new()
 var fish_scene = preload("res://scenes/fish.tscn")
+@export var FishDatas: Array[FishData] # add in editor UI
 
 @onready var time_since_last_check = 0;
-@onready var screen_size: Vector2 = get_viewport().get_visible_rect().size
+@onready var screensize: Vector2 = get_viewport().size
 
 # Potentially unnecessary since get_child_count() exists.
 #@onready var fish_count = 0
 
-const FISH_CAP = 5
-const TIME_BETWEEN_CHECKS = 0.05
+# I changed these to exports for when we make new stages - Watson
+@export var FISH_CAP = 5
+@export var TIME_BETWEEN_CHECKS = 0.05
 
 # @onready var generic_red_fish = load("res://assets/2d/placeholder fish/FishRed.png")
 
@@ -18,11 +20,9 @@ const TIME_BETWEEN_CHECKS = 0.05
 func _ready() -> void:
 	rng.randomize()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time_since_last_check += delta
-	# Check if fish cap has been met each interval.
 	if(time_since_last_check >= TIME_BETWEEN_CHECKS):
 		time_since_last_check = 0
 		if(get_child_count() < FISH_CAP):
@@ -31,7 +31,6 @@ func _process(delta: float) -> void:
 # Spawns a fish at a random location.
 func spawn_fish():
 	var new_fish = fish_scene.instantiate()
-	new_fish.screensize = 0
+	var pos := Vector2(rng.randf_range(0, screensize.x), rng.randf_range(0, screensize.y))
+	new_fish.set_values(pos, FishDatas.pick_random())
 	add_child(new_fish)
-	var initial_position: Vector2 = Vector2(random.randf_range(0, screen_size.x), random.randf_range(0, screen_size.y))
-	new_fish.set_initial_values(false, 60, 3, 40, initial_position, generic_red_fish)
